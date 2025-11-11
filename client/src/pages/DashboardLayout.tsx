520b68fa011e7230fcec085478107380c926fcb8
Neon-Nomad <bitepixelstudio@gmail.com>
2025-11-11 17:51:49 -0500

    fix: Add api to DashboardLayout context for child components

--- client/src/pages/DashboardLayout.tsx
+++ client/src/pages/DashboardLayout.tsx
@@ -73,7 +73,7 @@ export default function DashboardLayout() {
         </div>
       </div>
       <div className="mx-auto max-w-6xl px-6 py-10">
-        <Outlet context={{ tenant, setTenant }} />
+        <Outlet context={{ tenant, setTenant, api }} />
       </div>
     </div>
   );
